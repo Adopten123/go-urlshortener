c73f247d85966c500f574c7961802b7a427f5ba6
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2026-01-14 17:28:36 +0300

    todo list

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// TODO: init config. Libs: cleanenv
+	// TODO: init logger. Libs: log/slog
+	// TODO: init storage. Libs: sqlite
+	// TODO: init router. Libs: chi, "chi render"
+	// TODO: run server.
+}
