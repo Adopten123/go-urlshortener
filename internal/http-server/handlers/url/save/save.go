d2293c7c701643a2c0d29e0db0b88325ef312cc7
Alexander Mikhailov <alexandermikhailov0505@gmail.com>
2026-01-15 20:35:11 +0300

    add random alias generator

---
+++ internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,70 @@
+package save
+
+import (
+	"errors"
+	"go-urlshortener/internal/lib/api/response"
+	"go-urlshortener/internal/lib/logger/sl"
+	"go-urlshortener/internal/lib/random"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+)
+
+type Request struct {
+	URL   string `json:"urls" validate:"required,url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	response.Response
+	Alias string `json:"alias,omitempty"`
+}
+
+const aliasLength = 6 // TODO: move to config
+
+type URLSaver interface {
+	SaveURL(urlToSave string, alias string) (int64, error)
+}
+
+func New(logger *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save.New"
+
+		logger = logger.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			logger.Error("failed to decode request", "error", sl.Err(err))
+			render.JSON(w, r, response.Error("failed to decode request"))
+			return
+		}
+		logger.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+			logger.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, response.ValidationError(validateErr))
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrAlreadyExists) {
+			logger.Info("url already exists", slog.String("url", req.URL))
+			render.JSON(w, r, response.Error("url already exists"))
+			return
+		}
+	}
+}
